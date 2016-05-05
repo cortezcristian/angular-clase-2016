550eba5526d5644cedb1bb3b94c67cb3d2a7d408
Cristian Cortez <cortez.cristian@gmail.com>
2016-05-05 20:17:17 -0300

    Adding generated site

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularClase2016App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularClase2016App
+ */
+angular.module('angularClase2016App')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
